53b7635af9dd6e9fb66b1fa717a652374a4801d9
Baljeet Singh <bsbaljeetsingh9999@gmail.com>
2026-07-03 01:10:13 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+// import express from "express";
+
+// const app = express();
+
+// async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error");
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log("App is listening on port : ", process.env.PORT);
+//     });
+//   } catch (error) {
+//     console.log("Error: ", error);
+//     throw error;
+//   }
+// };
